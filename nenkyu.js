3988b1386f601f8fecd4f9991be17835e98ddf97
Karen Xie <karen-x@outlook.com>
2021-12-21 15:57:49 +0900

    Static hosting

--- nenkyu.js
+++ nenkyu.js
@@ -1,16 +1,7 @@
 (function annualLeave() {
-    let date = prompt("Please enter date of leave (YYYY-MM-DD)");
-    if (date === null) {
-        alert("Please type in your date");
-        return;
-    }
-    
     const oneDayLeave = document.querySelector('input[type="radio"][name="1977"]');
     oneDayLeave.checked = true;
     oneDayLeave.onchange();
-
-    const dateOfLeave = document.querySelector('.assessment_report_datepicker');
-    dateOfLeave.value = date;
 
     const nenkyu = document.querySelector('input[type="radio"][name="1979"]');
     nenkyu.checked = true;
