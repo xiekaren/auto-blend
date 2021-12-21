86a9e87fe47f56e9752e9810e8ff96437b932477
Karen Xie <karen-x@outlook.com>
2021-12-21 12:59:13 +0900

    Reduce nesting

--- annual-leave.js
+++ annual-leave.js
@@ -2,21 +2,22 @@
     let date = prompt("Please enter date of leave (YYYY-MM-DD)");
     if (date === null) {
         alert("Please type in your date");
-    } else {
-        const oneDayLeave = document.querySelector('input[type="radio"][name="1977"]');
-        oneDayLeave.checked = true;
-        oneDayLeave.onchange();
-    
-        const dateOfLeave = document.querySelector('.assessment_report_datepicker');
-        dateOfLeave.value = date;
-    
-        const nenkyu = document.querySelector('input[type="radio"][name="1979"]');
-        nenkyu.checked = true;
-    
-        const reasonForLeave = document.getElementsByTagName('textarea');
-        reasonForLeave[0].value = "私事の都合のため";
-    
-        const noClasses = document.querySelector('input[type="radio"][value="無"]');
-        noClasses.checked = true;
+        return;
     }
+    
+    const oneDayLeave = document.querySelector('input[type="radio"][name="1977"]');
+    oneDayLeave.checked = true;
+    oneDayLeave.onchange();
+
+    const dateOfLeave = document.querySelector('.assessment_report_datepicker');
+    dateOfLeave.value = date;
+
+    const nenkyu = document.querySelector('input[type="radio"][name="1979"]');
+    nenkyu.checked = true;
+
+    const reasonForLeave = document.getElementsByTagName('textarea');
+    reasonForLeave[0].value = "私事の都合のため";
+
+    const noClasses = document.querySelector('input[type="radio"][value="無"]');
+    noClasses.checked = true;
 })();
